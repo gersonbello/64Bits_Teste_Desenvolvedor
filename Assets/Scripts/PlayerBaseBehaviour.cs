21bd5484035303d385f73d492020b1753f48379c
Gerson Bello de Goes Filho <gersonbellodegoes@gmail.com>
2023-03-04 18:50:16 -0300

    TEST64BITS-6 Adicionado pilhagem e movimentação básica dos npcs ao serem derrubados

--- Assets/Scripts/PlayerBaseBehaviour.cs
+++ Assets/Scripts/PlayerBaseBehaviour.cs
@@ -7,18 +7,45 @@ public class PlayerBaseBehaviour : MonoBehaviour
     private CharacterController charController;
     private Animator charAnimator;
 
-    [Header("Configura��es")] [Space(5)]
+    [Header("Player Settings")]
+    [Space(5)]
 
-    [SerializeField] [Tooltip("Velocidade em que o personagem se move")]
+    [SerializeField]
+    [Tooltip("Velocidade em que o personagem se move")]
     private float moveSpeed;
-    [SerializeField] [Tooltip("Velocidade em que o personagem se rotaciona")]
+    [SerializeField]
+    [Tooltip("Velocidade em que o personagem se rotaciona")]
     private float rotateSpeed;
 
-    [SerializeField] [Tooltip("For�a do soco")]
+    [SerializeField]
+    [Tooltip("Força do soco")]
     private float punchForce;
+
+    [Header("Stack Settings")]
+    [Space(5)]
+
+    [SerializeField]
+    [Tooltip("Força do soco")]
+    private Transform stackPosition;
+
+    [SerializeField]
+    [Tooltip("Quantidade máxima da pilha de npcs nas costas do personagem")]
+    private int maxEnemyStack;
+
+    [SerializeField]
+    [Tooltip("Tempo até o inimigo ser adicionado à pilha")]
+    private float stackWaitTime;
+
+    [SerializeField]
+    [Tooltip("Velocidade em que o inimigo é adicionado à pilha")]
+    private float stackSpeed;
 
     private Vector2 joystickAxis;
     private Vector3 moveDirection;
+
+
+    private List<Transform> enemyToStack = new List<Transform>();
+    private Stack<Transform> enemyStack = new Stack<Transform>();
 
     private void Awake()
     {
@@ -34,7 +61,8 @@ public class PlayerBaseBehaviour : MonoBehaviour
     private void FixedUpdate()
     {
         charController.Move(GetMovementDirection() * moveSpeed * Time.deltaTime);
-        if(joystickAxis.magnitude != 0) RotateCharacterToDirection(GetMovementDirection());
+        if (joystickAxis.magnitude != 0) RotateCharacterToDirection(GetMovementDirection());
+        SetStackPosition();
     }
 
     /// <summary>
@@ -75,19 +103,65 @@ public class PlayerBaseBehaviour : MonoBehaviour
     {
     }
 
-    private void OnTriggerEnter(Collider hit)
+    private void OnTriggerEnter(Collider col)
     {
-        if (hit.transform.CompareTag("Enemy"))
+        if (col.transform.CompareTag("Enemy"))
         {
             charAnimator.SetTrigger("punch");
-            hit.transform.GetComponent<Animator>().enabled = false;
-            Rigidbody [] enemyRigs = hit.transform.GetComponentsInChildren<Rigidbody>();
-            foreach (Rigidbody eRig  in enemyRigs)
+            col.transform.GetComponent<Animator>().enabled = false;
+            Rigidbody[] enemyRigs = col.transform.GetComponentsInChildren<Rigidbody>();
+            StartCoroutine(KillEnemy(enemyRigs, col));
+        }
+    }
+
+    private IEnumerator KillEnemy(Rigidbody[] enemyRigs, Collider col)
+    {
+        yield return new WaitForSeconds(0.15f);
+        foreach (Rigidbody eRig in enemyRigs)
+        {
+            eRig.isKinematic = false;
+            eRig.AddForce((transform.forward + Vector3.up * .5f) * punchForce);
+        }
+        col.GetComponent<Rigidbody>().isKinematic = true;
+        col.transform.gameObject.layer = 3;
+
+        yield return new WaitForSeconds(stackWaitTime);
+
+        enemyToStack.Add(col.transform.GetChild(0).transform);
+    }
+    /// <summary>
+    /// Movimenta npcs derrubados
+    /// </summary>
+    private void SetStackPosition()
+    {
+        // Movimenta os npcs derrubados até a pilha
+        foreach (Transform t in enemyToStack)
+        {
+            Vector3 newPos = stackPosition.position;
+            newPos.y += enemyStack.Count * .8f;
+            t.position = Vector3.Lerp(t.position, newPos, stackSpeed * Time.deltaTime);
+            if(Vector3.Distance(t.position, newPos) < 1f)
             {
-                eRig.isKinematic = false;
-                eRig.AddForce((transform.forward + Vector3.up * .5f) * punchForce);
-                hit.transform.gameObject.layer = 3;
+                enemyToStack.Remove(t);
+                enemyStack.Push(t.parent);
+                t.parent.position = newPos;
+                t.position = newPos;
+                Rigidbody rig = t.GetComponent<Rigidbody>();
+                rig.isKinematic = true;
+                rig.freezeRotation = true;
+                break;
             }
+        }
+
+        // Movimenta npcs na pilha para a posição correta de acordo com o index
+        int stackIndex = enemyStack.Count;
+
+        foreach (Transform t in enemyStack)
+        {
+            Vector3 newPos = stackPosition.position;
+            newPos.y += stackIndex * .8f;
+            t.position = Vector3.Lerp(t.position, newPos, stackSpeed * Time.deltaTime);
+            stackIndex--;
         }
     }
 }
